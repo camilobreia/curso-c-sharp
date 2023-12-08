b3e6c64fa66721c0803a3292a281ec554239092b
Camilo Breia <clbreia@gmail.com>
2023-12-08 13:40:03 -0300

    olá mundo - console

---
+++ ConsoleEx000/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console; //serve para não ter que escrever "CONSOLE" sempre
+
+// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine("Hello, World!"); => já veio automaticamente
+
+// System.Console.Out.WriteLine("Olá, Mundo!"); => essa é a maneira completa
+// System.Console.ReadKey(); => serve para caso o console fechasse automaticamente
+
+// Console.WriteLine("Olá Mundo!");
+
+WriteLine("Olá Mundo!"); // modo mais simples usando static system.console
+ReadKey();
