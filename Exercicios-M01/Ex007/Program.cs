b0920b00655d2eceb9eb081e8efb01be60304866
Camilo Breia <clbreia@gmail.com>
2024-01-04 23:35:32 -0300

    exercícios

---
+++ Exercicios-M01/Ex007/Program.cs
@@ -0,0 +1,23 @@
+﻿Random ger = new Random();
+
+int i = 0;
+int f = 0;
+
+Console.WriteLine("SORTEADOR DE NÚMERO");
+Console.WriteLine("----------------------");
+Console.Write("Início: ");
+int.TryParse(Console.ReadLine(), out i);
+Console.Write("Fim: ");
+int.TryParse(Console.ReadLine(), out f);
+Console.WriteLine("----------------------");
+
+Console.SetCursorPosition(0, 5);
+Console.WriteLine("SORTEANDO...");
+Thread.Sleep(2000);
+
+int r = ger.Next(i, f + 1);
+
+Console.SetCursorPosition(0, 5);
+Console.WriteLine($"Entre {i} e {f} sorteei o valor {r}");
+
+Console.ReadKey();
---
+++ Exercicios-M01/Ex008/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ex008
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSortear_Click(object sender, RoutedEventArgs e)
+        {
+            int n1, n2;
+            int.TryParse(txtInic.Text, out n1);
+            int.TryParse(txtFim.Text, out n2);
+
+            Random gerador = new Random();
+            int ger = gerador.Next(n1, n2+1);
+
+            lblMsg.Visibility = Visibility.Visible;
+            lblMsg.Content = $"Sorteando de {n1} a {n2}...";
+            await Task.Delay(2000);
+            lblMsg.Content = $"Sorteei o valor {ger}";
+        }
+    }
+}
